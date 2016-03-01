704292628dddd72c924390d9f0f1756bf2abf0be
Nicholas McGlincy <nmcglincy@gmail.com>
2016-03-01 09:42:04 -0800

    took me a while, but I got it debugged

--- passwd-to-directory.sh
+++ passwd-to-directory.sh
@@ -11,5 +11,33 @@ umask 077
 PERSON=/tmp/pd.key.person.$$
 OFFICE=/tmp/pd.key.office.$$
 TELEPHONE=/tmp/pd.key.telephone.$$
-USED=/tmp/pd.key.user.$$
+USER=/tmp/pd.key.user.$$
 
+trap "exit 1"												HUP INT PIPE QUIT TERM
+trap "rm -f $PERSON $OFFICE $TELEPHONE $USER" EXIT
+
+awk -F: '{ print $1 ":" $5 }' > $USER
+
+# more $USER
+
+sed -e 's=/.*==' -e 's=^\([^:]*\):\(.*\) \([^ ]*\)=\1:\3, \2=' < $USER | sort > $PERSON
+
+# printf " \n"
+# more $PERSON
+
+sed -e 's=^\([^:]*\):[^/]*/\([^/]*\).*$=\1:\2=' < $USER | sort > $OFFICE
+
+# printf " \n"
+# more $OFFICE
+
+sed -e 's=^\([^:]*\):[^/]*/[^/]*/\([^/]*\)=\1:\2=' <$USER | sort > $TELEPHONE
+
+# printf " \n"
+# more $TELEPHONE
+# printf " \n"
+
+join -t: $PERSON $OFFICE \
+	| join -t: - $TELEPHONE \
+	| cut -d: -f2- \
+	| sort -t: -k1,1 -k2,2 -k3,3 \
+	| awk -F: '{ printf("%-39s\t%s\t%s\n", $1, $2, $3)}'
