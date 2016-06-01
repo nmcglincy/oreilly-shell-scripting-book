de513fb67e3946aa3faf70f0582d48f46b911b18
Nicholas McGlincy <nmcglincy@gmail.com>
2016-06-01 12:31:24 -0700

    in the middle of testing

--- pathfind.sh
+++ pathfind.sh
@@ -52,3 +52,72 @@ EXITCODE=0
 PROGRAM=`basename $0`
 VERSION=1.0
 
+while test $# -gt 0
+do
+	case $1 in
+		--all | --al | --a | -all | -al | -a )
+			all=yes
+			;;
+		--help | --hel | --he | --h | '--?' | -help | -hel | -he | -h | '-?' )
+			usage_and_exit 0
+			;;
+		--version | --versio | --versi | --vers | --ver | --ve | --v | \
+		-version | -versio | -versi | -vers | -ver | -ve | -v )
+			version
+			exit 0
+			;;
+		-*)
+			error "Unrecognized option: $1"
+			;;
+		*)
+			break
+			;;
+	esac
+	shift
+done
+
+envar="$1"
+test $# -gt 0 && shift
+
+test "x$envar" = "xPATH" && envar=OLDPATH
+
+dirpath=`eval echo '${'"$envar"'}' 2>/dev/null | tr :  ' '`
+
+# sanity checks for error conditions
+if test -z "$envar"
+then
+	error Environment variable missing or empty
+elif test "x$dirpath" = "x$envar"
+then
+	error "Broken sh on this platform: cannot expand $envar"
+elif test -z "$dirpath"
+then
+	error Empty directory search path
+elif test $# -eq 0
+then
+	exit 0
+fi
+
+for pattern in "$@"
+do
+	result=
+	for dir in $dirpath
+	do
+		for file in $dir/$pattern
+		do
+			if test -f "$file"
+			then
+				result="$file"
+				echo $result
+				test "$all" = "no" && break 2
+			fi
+		done
+	done
+	test -z "$result" && warning "$pattern: not found"
+done
+
+#  Limit exit status to common Unix practice
+# this errors on some test command and I'm not sure why 20160601
+test $EXITCODE -gt 125 && EXITCODE=125
+
+exit $EXITCODE
